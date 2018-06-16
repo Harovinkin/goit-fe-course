ba6819dcc5e17f526b8722854a5b3d24c09a5a8d
Dmitriy Harovinkin <kharovinkin@gmail.com>
2018-06-16 08:29:12 +0300

    mod_01 first working code

---
+++ javascript/module_01/js/index.js
@@ -0,0 +1,32 @@
+'use srtict'
+
+const ADMIN_LOGIN = 'admin';
+
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+
+const userCancel = 'Отменено пользователем!';
+
+const accessErr = 'Доступ запрещен!';
+
+const accessYes = 'Добро пожаловать!';
+
+const userLog = prompt('Введите Логин');
+
+
+if (userLog === ADMIN_LOGIN) {
+
+  const userPass = prompt('Введите Пароль');
+  
+    if (userPass === ADMIN_PASSWORD) {
+      alert( accessYes );
+    } else if (userPass === null) {
+      alert( userCancel );
+    } else {
+      alert( accessErr );
+    }
+
+} else if (userLog === null) {
+  alert( userCancel );
+} else {
+  alert( accessErr );
+}
