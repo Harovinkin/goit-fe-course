631185ba96e3ef35d09b8d09321273657bcdfc5b
Dmitriy Harovinkin <kharovinkin@gmail.com>
2018-06-25 13:42:59 +0300

    mod-02-add first commit of working code

---
+++ javascript/module_02_add/js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3;
+
+out: do {
+  const userPassword = prompt('Введите свой пароль', '');
+
+  if (userPassword !== null) {
+    for (const val of passwords) {
+      if (userPassword === val) {
+        alert('Добро пожаловать!');
+        break out;
+      }
+    }
+    
+    attempts -= 1;
+
+    if (attempts > 0) {
+      alert(`"Неверный пароль, у вас осталось ${attempts} попыток"`);
+    } else {
+      break out;
+    }
+
+  } else {
+    break out;
+  }
+  
+} while (attempts);
+
